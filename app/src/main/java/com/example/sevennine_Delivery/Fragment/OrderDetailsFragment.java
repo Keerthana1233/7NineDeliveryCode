262ab02152e28ff70bb7c9051662c4f58731ddc4
himabindhu <himabindhu.chandhragiri@gmail.com>
2020-06-02 15:56:47 +0530

    Trackercode and orders code pushed to git _himabindhu_02-06-2020eod

--- app/src/main/java/com/example/sevennine_Delivery/Fragment/OrderDetailsFragment.java
+++ app/src/main/java/com/example/sevennine_Delivery/Fragment/OrderDetailsFragment.java
@@ -28,7 +28,7 @@ import java.util.List;
 
 
 public class OrderDetailsFragment extends Fragment {
-//commit code
+//commit code eod
     public static List<OrderDetailBean> newOrderBeansList = new ArrayList<>();
     public static RecyclerView recyclerView;
     LinearLayout back_feed;
